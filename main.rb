8ddf07f81156a907082d90f4c5625b0e1a2950de
Michelle Garcia <thelionpear@gmail.com>
2018-05-10 19:15:37 -0600

    adds the print answers function. also makes it so user can't add duplicate answers

--- main.rb
+++ main.rb
@@ -50,6 +50,26 @@ def add_answer
 	puts "What answer would you like to add?"
 
 	answer_input = gets.strip
+#duplicate found is made false to start with 
+	duplicate_found = false
+# goes through the @answer_bank array and turns each thing in it into the variable "item"
+# then checks if that is equal to the thing the user submitted
+# it sets duplicate found to true then it triggers that function
+# if it remains false, it keeps going through the function
+	@answer_bank.each{ |item|
+		if answer_input == item.answer 
+			duplicate_found = true 
+		end
+	}
+# if the answer the user gives triggers the duplicate found function, it 
+# puts the statement and takes them back to the beginning of the add answer function 
+# we had to put the return command inside a separate function because it can't be with 
+# the lambda (the pipe thingy) above 
+	if duplicate_found
+		puts "You cannot add an answer that already exists."
+		add_answer
+		return
+	end
 # creating a local variable called answer that takes user input
 # and also adds on false to discriminate it from the default answers
 	answer = Answer.new(answer_input, false)
@@ -78,6 +98,13 @@ def reset_answer
 			puts "Not a valid response."
 			reset_answer 
 	end 
+end 
+
+def print_answers 
+	@answer_bank.each{ |item|
+		puts item.answer
+	}
+	menu 
 end 
 
 menu 
