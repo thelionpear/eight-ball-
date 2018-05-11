0089fe95cb2435018b1560398452db6ceee4b62d
Michelle Garcia <thelionpear@gmail.com>
2018-05-10 18:50:58 -0600

    brought back answers class and made it work. Whole program works.

--- answers.rb
+++ answers.rb
@@ -1,10 +1,19 @@
+
+
+
 class Answer
-	attr_accessor :answer
 
-	def initialize
-		@answer = answer 	
+	#accessor allows read and write of this class
+	attr_accessor :answer, :is_default 
+#initializes itself when called upon and 
+# asks for the variable answer to be passed into it
+# also looks for true or false in 
+	def initialize(answer, is_default)
+		@answer = answer 
+		@is_default = is_default 	
 	end
-
+# creates a function that will look in the main script
+# 
 	def give_answer
 		puts @answer.sample
 	end
--- main.rb
+++ main.rb
@@ -6,55 +6,78 @@
 # abiltiy print all answer
 # exit
 
-#require_relative 'answers'
+require_relative 'answers'
 
-class Main
+# initializes an array that has preset answers 
+# it passes those answers into the answers class
 
-	attr_accessor :answers
-	answer_bank = ['yes', 'no', 'maybe']
-	
-	def initialize
-		@answers = Array.new(answer_bank)
-	end
-	
+@answer_bank = [
+	Answer.new('yes', true),
+	Answer.new('no', true),
+	Answer.new('maybe', true)
+]
+
 
 def menu
 	puts "Welcome to the magic eight ball"
 	puts "What would you like to know?"
+	puts "If you don't want to continue playing, type Quit"
 
 	user_selection
 end
 
-def add_answer
-	puts "What answer would you like to add?"
 
-	answer_input = gets.strip
-
-	@answers << answer_input
-
-	puts "you just added #{@answers.last}"
-
-	menu
-end
 
 def user_selection
 	question = gets.strip.downcase
 
 	case question
-	when "add answer"
-		add_answer
-	when "reset answers"
-		reset_answer
-	when "see all answers"
-		print_answers
-	when "quit"
-		exit
-	else
-		answer = Answer.new
-		puts "#{@answer_bank.sample}"
+		when "add answer"
+			add_answer
+		when "reset answers"
+			reset_answer
+		when "see all answers"
+			print_answers
+		when "quit"
+			exit
+		else
+			puts @answer_bank.sample.answer 
+			menu 
 	end
-	menu
-end
+end 
+
+def add_answer
+	puts "What answer would you like to add?"
+
+	answer_input = gets.strip
+# creating a local variable called answer that takes user input
+# and also adds on false to discriminate it from the default answers
+	answer = Answer.new(answer_input, false)
+# shovels the new variable "answer" onto the end of the answer_bank array
+	@answer_bank << answer
+
+	puts "You just added #{@answer_bank.last.answer}"
+
+	menu 
 end
 
-Main.new
+def reset_answer 
+	puts "Would you like to go back to the default answers? Y or N?"
+	response = gets.chomp 
+	case
+		when response == "Y" || response == "y" 
+# selects only items that have "true" for "is_default" to keep in array
+			@answer_bank = @answer_bank.select {|item|
+				item.is_default == true
+			}
+			menu 
+		when response == "N" || response == "n"
+			puts "Okay, keeping your customized answers."
+			menu 
+		else 
+			puts "Not a valid response."
+			reset_answer 
+	end 
+end 
+
+menu 
